f857f8913ea561f078e4dc856dcda2aeee3c5b18
SergioEstebanPi <sepi_147@hotmail.com>
2019-01-20 02:46:06 -0500

    Agrego test donde se realiza el consumo de los endpoints del servicio

--- src/main/java/com/prueba/calculadorasoap/CalculadoraSOAP.java
+++ src/main/java/com/prueba/calculadorasoap/CalculadoraSOAP.java
@@ -5,12 +5,41 @@
  */
 package com.prueba.calculadorasoap;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import org.tempuri.Calculator;
+import org.tempuri.CalculatorSoap;
+
 /**
  *
  * @author mac
  */
 public class CalculadoraSOAP {
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        try {
+            System.out.println("hello");
+            URL url = new URL("http://www.dneonline.com/calculator.asmx?WSDL");
+            //URL url = new URL("http://localhost:7779/calculator");
+            
+            // 1st argument service URI, refer to wsdl document above NAMESPACE
+            // 2nd argument is service name, refer to wsdl document above
+            QName qname = new QName("http://tempuri.org/", "Calculator");
+            //QName qname = new QName("http://calculator/", "CalculatorImplService");
+            Service service = Service.create(url, qname);
+            System.out.println(service.getServiceName());
+            CalculatorSoap calculator = service.getPort(CalculatorSoap.class);
+            
+            System.out.println(calculator.add(8, 6));
+            System.out.println(calculator.subtract(8, 6));
+            System.out.println(calculator.multiply(8, 6));
+            System.out.println(calculator.divide(8, 6));
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(CalculadoraSOAP.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
